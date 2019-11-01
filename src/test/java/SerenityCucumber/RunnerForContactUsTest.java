5c5adf2d7b022d3e28ade7bffd09123539001405
levgren <jekaobodianskyi@gmail.com>
2019-11-01 16:06:16 +0200

    +

--- src/test/java/SerenityCucumber/RunnerForContactUsTest.java
+++ src/test/java/SerenityCucumber/RunnerForContactUsTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/featureFiles/contactUs.feature",
-        plugin = {"pretty", "json:target/cucumber-report/cucumber.json"},
+        plugin = {"html:target/cucumber-html-report",
+                "json:target/cucumber-report/cucumber.json"},
         monochrome = true)
 public class RunnerForContactUsTest {
 }
